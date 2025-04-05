ea57a0cc314b3cf814a6ede4c7ec20c60d45d3bd
deuskane <deuskane@gmail.com>
2025-04-05 09:35:00 +0200

    Add algo binary / one hot

--- src/pbi_wrapper_target.vhd
+++ src/pbi_wrapper_target.vhd
@@ -6,7 +6,7 @@
 -- Author     : Mathieu Rosière
 -- Company    : 
 -- Created    : 2014-06-03
--- Last update: 2025-03-31
+-- Last update: 2025-04-05
 -- Platform   : 
 -- Standard   : VHDL'87
 -------------------------------------------------------------------------------
@@ -17,7 +17,8 @@
 -- Revisions  :
 -- Date        Version  Author   Description
 -- 2014-06-03  1.0      mrosiere Created
--- 2025-08_03  1.1      mrosiere Use unconstrainted pbi
+-- 2025-08-03  1.1      mrosiere Use unconstrainted pbi
+-- 2025-04-05  1.2      mrosiere Add Algo (binary/one-hot)
 -------------------------------------------------------------------------------
 
 library ieee;
@@ -25,14 +26,16 @@ use     ieee.std_logic_1164.all;
 use     ieee.numeric_std.all;
 library work;
 use     work.pbi_pkg.all;
-use     work.string_pkg.all;
+use     work.logic_pkg.all;
+use     work.convert_pkg.all;
 
 entity pbi_wrapper_target is
   -- =====[ Parameters ]==========================
   generic (
     SIZE_DATA      : natural := 8;
     SIZE_ADDR_IP   : natural := 0;
-    ID             : std_logic_vector (PBI_ADDR_WIDTH-1 downto 0) := (others => '0')
+    ID             : std_logic_vector (PBI_ADDR_WIDTH-1 downto 0) := (others => '0');
+    ALGO           : string  := "binary" -- "binary" / "one_hot"
      );
   -- =====[ Interfaces ]==========================
   port (
@@ -54,7 +57,8 @@ architecture rtl of pbi_wrapper_target is
   constant SIZE_ADDR       : natural := pbi_ini_i.addr'length;
   constant SIZE_ADDR_ID    : natural := SIZE_ADDR-SIZE_ADDR_IP;
   constant CST0            : std_logic_vector(pbi_tgt_o.rdata'range) := (others => '0');
-                        
+  constant IDX             : integer := onehot_to_integer(ID);
+    
   signal   pbi_id          : std_logic_vector(SIZE_ADDR_ID-1 downto 0);
   signal   tgt_id          : std_logic_vector(SIZE_ADDR_ID-1 downto 0);
            
@@ -81,12 +85,20 @@ begin  -- rtl
   -- Chip Select
   -----------------------------------------------------------------------------
   -- Don't use Alias to see this signal in gtkwave
-  pbi_id             <= ini_addr(SIZE_ADDR   -1 downto SIZE_ADDR_IP);
-  tgt_id             <= ID      (SIZE_ADDR   -1 downto SIZE_ADDR_IP);
-  tgt_addr           <= ini_addr(SIZE_ADDR_IP-1 downto 0);
   
-  cs                 <= ini_cs when (pbi_id = tgt_id) else
-                        '0';
+  gen_binary: if ALGO="binary"
+  generate
+    pbi_id             <= ini_addr(SIZE_ADDR   -1 downto SIZE_ADDR_IP);
+    tgt_id             <= ID      (SIZE_ADDR   -1 downto SIZE_ADDR_IP);
+    
+    cs                 <= ini_cs when (pbi_id = tgt_id) else
+                          '0';
+  end generate gen_binary;
+    
+  gen_one_hot: if ALGO="one_hot"
+  generate
+    cs                 <= ini_cs and ini_addr(IDX);
+  end generate gen_one_hot;
 
   
   -----------------------------------------------------------------------------
@@ -98,6 +110,8 @@ begin  -- rtl
   ini_we             <= pbi_ini_i.we   ;
   ini_addr           <= pbi_ini_i.addr ;
   ini_wdata          <= pbi_ini_i.wdata;
+
+  tgt_addr           <= ini_addr(SIZE_ADDR_IP-1 downto 0);
   
   -----------------------------------------------------------------------------
   -- To Bus
@@ -124,7 +138,14 @@ begin  -- rtl
   process is
   begin  -- process
 
-    report "Target["&to_hstring(ID)&"] Address : "&integer'image(SIZE_ADDR_IP) severity note;  
+    report "Target["&to_hstring(ID)&"] Address : "&integer'image(SIZE_ADDR_IP) severity note;
+
+    if (ALGO = "one_hot")
+    then
+      report "  * Index : " &integer'image(IDX) severity note;
+      
+    end if;
+    
 
     wait;
   end process;
